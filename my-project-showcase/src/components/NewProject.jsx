93c39c558785bd351f4c18cdc4d583178f64fd67
MutumaM <jaydenmwongera@gmail.com>
2026-05-03 23:59:16 +0300

    card

---
+++ my-project-showcase/src/components/NewProject.jsx
@@ -0,0 +1,14 @@
+function NewProject (){
+    return(
+        <>
+            <h2>Add project</h2>
+            <h3>Title</h3>
+            <input />
+            <h3>Description</h3>
+            <input />
+            <button>Add</button>
+        </>
+    )
+}
+
+export default NewProject;
