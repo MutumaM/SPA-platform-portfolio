1dbc2751a54cff1cb6120b7ba7b3390a0c2a1e96
MutumaM <jaydenmwongera@gmail.com>
2026-05-05 08:57:40 +0300

    stylingnotdone

--- my-project-showcase/src/App.jsx
+++ my-project-showcase/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./App.css";
 import NewProject from "./components/NewProject";
 import FilterProject from "./components/FilterProject";
 import ProjectList from "./components/ProjectList";
--- my-project-showcase/src/components/FilterProject.jsx
+++ my-project-showcase/src/components/FilterProject.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 function FilterProject() {
+    const [searchText, setSearchText]= React.useState('')
+  
+  const saveSearchedProject = (event) => {
+    setSearchText(event.target.value)
+  }
+
     return(
         <div className='filter-project'>
+            <input placeholder="Search projects" onChange = {saveSearchedProject} />
+            <button>Search</button>
         </div>
     )
 }
